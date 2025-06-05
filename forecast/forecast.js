932a643a345cc6a8f8487594375f6bc4a3474b43
jessimeteo <jessica.laengle@uibk.ac.at>
2025-06-05 15:46:25 +0200

    Dateien für Vorhersage erstellt

---
+++ forecast/forecast.js
@@ -0,0 +1,36 @@
+/* Wind & Wetter Beispiel */
+
+// Innsbruck
+let ibk = {
+    lat: 47.267222,
+    lng: 11.392778
+};
+
+// Karte initialisieren
+let map = L.map("map").setView([ibk.lat, ibk.lng], 5);
+
+// thematische Layer
+let overlays = {
+    forecast: L.featureGroup().addTo(map),
+    wind: L.featureGroup().addTo(map),
+}
+
+// Layer Control
+let layerControl = L.control.layers({
+    "Openstreetmap": L.tileLayer.provider("OpenStreetMap.Mapnik"),
+    "Esri WorldTopoMap": L.tileLayer.provider("Esri.WorldTopoMap"),
+    "Esri WorldImagery": L.tileLayer.provider("Esri.WorldImagery").addTo(map)
+}, {
+    "Wettervorhersage MET Norway": overlays.forecast,
+    "ECMWF Windvorhersage": overlays.wind,
+}).addTo(map);
+
+// Maßstab
+L.control.scale({
+    imperial: false,
+}).addTo(map);
+
+let url = `https://dataset.api.hub.geosphere.at/v1/timeseries/forecast/chem-v2-1h-9km`
+let response = await fetch(url);
+let jsondata = await response.json();
+console.log(jsondata);

844dcbdfb59aaf82fae0fd2abc1a4abdeb6fdd1e
Johanna Schramm <johanna.schramm@student.uibk.ac.at>
2025-06-08 14:56:52 +0200

    overlays definiert und raso für innsbruck funktioniert, yeeey!

--- nowcast/main.js
+++ nowcast/main.js
@@ -12,12 +12,37 @@ let ibk = {
 // Karte initialisieren
 let map = L.map("map").setView([ibk.lat, ibk.lng], ibk.zoom);
 
+// Overlays definieren
+let overlays = {
+    raso: L.featureGroup().addTo(map),
+    lidar: L.featureGroup().addTo(map),
+    geosphere: L.featureGroup().addTo(map),
+    aws: L.featureGroup().addTo(map),
+    uibk: L.featureGroup().addTo(map),
+};
+
 // Hintergrund-Layer
 L.control.layers({
     "OpenStreetMap": L.tileLayer.provider("OpenStreetMap.Mapnik").addTo(map),
     "OpenTopoMap": L.tileLayer.provider("OpenTopoMap"),
-    "Esri WorldImagery": L.tileLayer.provider("Esri.WorldImagery")
-}).addTo(map);
+    "Esri WorldImagery": L.tileLayer.provider("Esri.WorldImagery"),
+    "BasemapAT Relief": L.tileLayer.provider('BasemapAT.terrain'),
+    "BasemapAT Oberfläche": L.tileLayer.provider('BasemapAT.surface'),
+},
+    {
+        "Radiosondes": overlays.raso,
+        "Lidar": overlays.lidar,
+        "Geosphere Stations": overlays.geosphere,
+        "AWS Stations": overlays.aws,
+        "UIBK Stations": overlays.uibk,
+
+    }
+
+
+).addTo(map);
+
+
+
 
 // Maßstab
 L.control.scale({
--- nowcast/raso.js
+++ nowcast/raso.js
@@ -1,2 +1,62 @@
 /*This is the js file for the radiosond visualisation*/
 
+
+const rasoData = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "id": 11120,
+      "properties": {
+        "name": "INNSBRUCK-FLUGHAFEN, AUSTRIA",
+        "launch_time": "00"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [11.355, 47.260]
+      }
+    }
+  ]
+};
+
+
+
+
+
+// Radiosondes
+async function loadRadiosond() {
+    let geojson = rasoData;
+    
+    L.geoJSON(geojson, {
+        attribution: 'Datenquelle: <a href= "https://data.wien.gv.at"> Stadt Wien </a>',
+        
+        pointToLayer: function (feature, latlng) {
+            console.log(feature.properties)
+            
+            return L.marker(latlng,
+                {
+                    icon: L.icon({
+                        iconUrl: './photo.png',
+                        iconAnchor: [16, 37],
+                        popupAnchor: [0, -37],
+                    })
+                }
+            );
+        },
+        onEachFeature: function (feature, layer) {
+            //console.log(feature.properties);
+            let date = "20250608";
+            let time = feature.properties.launch_time;
+            let id = feature.id;
+            let url = `https://weather.uwyo.edu/upperair/imgs/${date}${time}.${id}.skewt.png`;
+            console.log(url);
+            layer.bindPopup(`
+                <a href=${url} target="raso"><img src="${url}" alt="*" style="max-width: 500px; height: auto;"></a>
+                <h4>${feature.properties.name}</h4>
+                <a href="${url}" target="raso">Skew T Diagramm</a>
+            `);
+        }
+    }).addTo(overlays.raso);
+}
+
+loadRadiosond()

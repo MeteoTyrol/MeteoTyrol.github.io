5c993234178d016770f03be1264041d9090ac01a
Johanna Schramm <johanna.schramm@student.uibk.ac.at>
2025-06-12 13:42:35 +0200

    Datenquellle verschönert

--- nowcast/lidar.js
+++ nowcast/lidar.js
@@ -80,7 +80,7 @@ async function loadLidar(date) {
   let geojson = lidarData;
 
   L.geoJSON(geojson, {
-    attribution: 'Lidar Data: <a href= "https://ertel2.uibk.ac.at/lidar-uebersicht/"> UIBK </a> <a href= "https://portale.zamg.ac.at/umweltprofile/index.php"> Geosphere </a>',
+    attribution: 'Lidar Data: <a href= "https://ertel2.uibk.ac.at/lidar-uebersicht/"> UIBK</a>, <a href= "https://portale.zamg.ac.at/umweltprofile/index.php"> Geosphere </a>',
 
     pointToLayer: function (feature, latlng) {
       return L.marker(latlng,
